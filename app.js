336d8466e276ce7f4ef2a108e7d707cddd6810a3
bhushan354 <deshmukhumesh354@gmail.com>
2023-07-24 16:41:34 +0530

    Add HTML+CSS+JS linters

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const hamburgerOpenConst = document.querySelector(".hamburgerOpen");
-const hamburgerCloseConst = document.querySelector(".hamburgerClose");
-const menuPageConst = document.querySelector(".menuPage");
-const pageLinkConst = document.querySelectorAll(".pageLink");
+const hamburgerOpenConst = document.querySelector('.hamburgerOpen');
+const hamburgerCloseConst = document.querySelector('.hamburgerClose');
+const menuPageConst = document.querySelector('.menuPage');
+const pageLinkConst = document.querySelectorAll('.pageLink');
 
 function openMenu() {
-  menuPageConst.style.display = "flex";
-  menuPageConst.style.top = "0";
+  menuPageConst.style.display = 'flex';
+  menuPageConst.style.top = '0';
 }
 
 function closeMenu() {
-  menuPageConst.style.top = "-100%";
+  menuPageConst.style.top = '-100%';
 }
 
 function handlePageLinkClick() {
   closeMenu();
 
-  const targetSectionId = this.getAttribute("href");
+  const targetSectionId = this.getAttribute('href');
   const targetSection = document.querySelector(targetSectionId);
 
-  targetSection.scrollIntoView({ behavior: "smooth" });
+  targetSection.scrollIntoView({ behavior: 'smooth' });
 }
 
-hamburgerOpenConst.addEventListener("click", openMenu);
-hamburgerCloseConst.addEventListener("click", closeMenu);
+hamburgerOpenConst.addEventListener('click', openMenu);
+hamburgerCloseConst.addEventListener('click', closeMenu);
 
 pageLinkConst.forEach((item) => {
-  item.addEventListener("click", handlePageLinkClick);
+  item.addEventListener('click', handlePageLinkClick);
 });
