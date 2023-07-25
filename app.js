0e47fc89a93d54ccbc2182b51d423cae291e912a
bhushan354 <deshmukhumesh354@gmail.com>
2023-07-25 18:19:59 +0530

    update elements dynamically

--- app.js
+++ app.js
@@ -27,3 +27,91 @@ hamburgerCloseConst.addEventListener('click', closeMenu);
 pageLinkConst.forEach((item) => {
   item.addEventListener('click', handlePageLinkClick);
 });
+
+//popup mobile
+
+const projects = [
+  {
+    name: 'Multi Post Stories',
+    description: 'A daily selection of privately personalized reads...',
+    image: 'popupImg.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+];
+
+const mainDivOne = document.querySelector('.mainDiv');
+
+const mainDivTwo = document.createElement('div') ;
+mainDivTwo.setAttribute('class', 'firstDiv');
+mainDivOne.appendChild(mainDivTwo);
+
+const mainText = document.createElement('h1');
+mainText.textContent = projects[0].name;
+mainDivTwo.appendChild(mainText);
+
+const ul = document.createElement('ul');
+ul.setAttribute('class', 'tech');
+mainDivTwo.appendChild(ul);
+
+const li1 = document.createElement('li');
+li1.textContent = 'HTML';
+
+const li2 = document.createElement('li');
+li2.textContent = 'Bootstrap'; 
+
+const li3 = document.createElement('li');
+li3.textContent = 'Ruby on Rails';
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
