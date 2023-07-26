41fe41209873e6551daa3344e4dd7fda0d44d402
bhushan354 <deshmukhumesh354@gmail.com>
2023-07-26 07:11:50 +0530

    popup with displayPopup function

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ pageLinkConst.forEach((item) => {
 const projects = [
   {
     name: 'Multi Post Stories',
-    description: 'A daily selection of privately personalized reads...',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this lorem consists of many of the things whixh you have not been heard of including ',
     image: 'popupImg.png',
     technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
     liveLink: 'https://google.com',
@@ -89,7 +89,18 @@ const projects = [
   },
 ];
 
-const mainDivOne = document.querySelector('.mainDiv');
+
+
+function displayPopup() {
+
+  const mainDivOne = document.querySelector('.mainDiv');
+
+const iconImg = document.createElement('img');
+iconImg.setAttribute('src', 'close.png'); 
+iconImg.setAttribute('class', 'iconOnTop');
+
+
+mainDivOne.appendChild(iconImg);
 
 const mainDivTwo = document.createElement('div') ;
 mainDivTwo.setAttribute('class', 'firstDiv');
@@ -103,15 +114,145 @@ const ul = document.createElement('ul');
 ul.setAttribute('class', 'tech');
 mainDivTwo.appendChild(ul);
 
-const li1 = document.createElement('li');
-li1.textContent = 'HTML';
+projects[0].technologies.forEach((tech) => {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  button.setAttribute('class', 'techButton');
+  button.textContent = tech; 
+  li.appendChild(button); 
+  ul.appendChild(li); 
+});
 
-const li2 = document.createElement('li');
-li2.textContent = 'Bootstrap'; 
+const mainDivThree = document.createElement('Div');
+mainDivThree.setAttribute('class', 'grid');
+mainDivOne.appendChild(mainDivThree);
 
-const li3 = document.createElement('li');
-li3.textContent = 'Ruby on Rails';
+const popupImg = document.createElement('img');
+popupImg.setAttribute('class', 'image')
+mainDivThree.appendChild(popupImg);
 
-ul.appendChild(li1);
-ul.appendChild(li2);
-ul.appendChild(li3);
+const mainDivFour = document.createElement('div');
+mainDivFour.setAttribute('class', 'gridTwo');
+mainDivThree.appendChild(mainDivFour);
+
+const description = projects[0].description;
+
+
+const paragraph = document.createElement('p');
+paragraph.textContent = description;
+mainDivFour.appendChild(paragraph);
+
+
+const buttonDiv = document.createElement('div');
+buttonDiv.setAttribute('class', 'btnContainer')
+mainDivFour.appendChild(buttonDiv);
+
+const button1 = document.createElement('button');
+button1.setAttribute('class', 'btnLive');
+button1.textContent = 'See Live';
+const img1 = document.createElement('img');
+img1.setAttribute('src', 'seelive.png');
+button1.appendChild(img1);
+buttonDiv.appendChild(button1);
+
+const button2 = document.createElement('button');
+button2.setAttribute('class', 'btnSource');
+button2.textContent = 'See Source';
+const img2 = document.createElement('img');
+img2.setAttribute('src', 'pagesource.png');
+button2.appendChild(img2); 
+buttonDiv.appendChild(button2);
+
+mainDivOne.style.display= 'block'; 
+}
+
+// multistories
+
+function generateMultiPostPage() {
+  
+  const multiPostPage = document.createElement('div');
+  multiPostPage.setAttribute('class', 'multiPostPage');
+
+  
+  const image2 = document.createElement('div');
+  image2.setAttribute('class', 'image2');
+  multiPostPage.appendChild(image2);
+
+  
+  const img1 = document.createElement('img');
+  img1.setAttribute('src', 'mainImage.png');
+  img1.setAttribute('alt', 'mainimage1');
+  img1.setAttribute('class', 'mainimage1');
+  image2.appendChild(img1);
+
+
+  const img2 = document.createElement('img');
+  img2.setAttribute('src', 'mainImage.png');
+  img2.setAttribute('alt', 'mainimage2');
+  img2.setAttribute('class', 'mainimage2');
+  image2.appendChild(img2);
+
+
+  const rightSection = document.createElement('div');
+  rightSection.setAttribute('class', 'rightSection');
+  multiPostPage.appendChild(rightSection);
+
+
+  const h2 = document.createElement('h2');
+  h2.setAttribute('class', 'multiPostStories');
+  h2.textContent = 'Multi-Post Stories';
+  rightSection.appendChild(h2);
+
+
+  const p = document.createElement('p');
+  p.setAttribute('class', 'pageInformation');
+  p.textContent =
+    "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.";
+  rightSection.appendChild(p);
+
+
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'techStacks');
+  rightSection.appendChild(ul);
+
+
+  const techStacks = ['CSS', 'HTML', 'Bootstrap', 'Ruby'];
+
+  techStacks.forEach((stack) => {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    button.setAttribute('class', 'stackButton');
+    button.setAttribute('type', 'button');
+    button.textContent = stack;
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+
+ 
+  const projectButton = document.createElement('div');
+  projectButton.setAttribute('class', 'projectButton');
+  rightSection.appendChild(projectButton);
+
+
+  const firstProjectButton = document.createElement('button');
+  firstProjectButton.setAttribute('class', 'firstProjectButton');
+  firstProjectButton.setAttribute('type', 'button');
+  firstProjectButton.textContent = 'See Project';
+  projectButton.appendChild(firstProjectButton);
+
+
+  return multiPostPage;
+}
+
+const generatedMultiPostPage = generateMultiPostPage();
+const multiPostContainer = document.querySelector('.multiPostContainer');
+multiPostContainer.appendChild(generatedMultiPostPage);
+
+// eventlistner for popup click
+multiPostContainer.addEventListener('click', function (event) {
+  if (event.target.matches('.firstProjectButton')) {
+    displayPopup();
+  }
+});
+
+
